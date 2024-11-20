7d1670ef7cf365d4b96ed080682cdb7d83f00a4b
herprojectfiles <antweb777@gmail.com>
2024-11-20 15:40:33 +0000

    Create faq.js

---
+++ js/faq.js
@@ -0,0 +1,12 @@
+// faq.js
+
+$(document).ready(() => {
+    // Initially hide all answers
+    $('.faq-item p').hide();
+
+    // Toggle answer visibility on question click
+    $('.faq-item h2').on('click', function () {
+        $(this).next('p').slideToggle(300);
+        $(this).toggleClass('active');
+    });
+});
