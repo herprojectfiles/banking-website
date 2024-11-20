c096bfee6072c8b9316b83c4bf6f3f8bb6f9c07f
herprojectfiles <antweb777@gmail.com>
2024-11-20 15:42:20 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// main.js
+
 $(document).ready(() => {
-    $('body').hide().fadeIn(1000); // Fade in effect on page load
+    // Fade-in effect for the body content
+    $('body').hide().fadeIn(1000);
+
+    // Smooth scroll for navbar links
+    $('.navbar a').on('click', function (e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate(
+                { scrollTop: $(hash).offset().top },
+                800
+            );
+        }
+    });
 });
